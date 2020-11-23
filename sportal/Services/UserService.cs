81f54fba182ad155bcb523f3fea1e33916aa757c
Jesse Paris <jesse.paris@qlik.com>
2020-11-23 15:13:20 -0800

    adding users in bcase64, removing old code and changing default launch to 8080

--- sportal/Services/UserService.cs
+++ sportal/Services/UserService.cs
@@ -15,59 +15,66 @@ namespace sportal.Services
 		private static string DEFAULT_USERS =
 @"[
 {
-    'subject': 'SaaSP-e797d094-716a-436d-a56b-e67144c5fbb4',
+    'subject': 'sportal-e797d094-716a-436d-a56b-e67144c5fbb4',
     'name': 'Emily Tiernen',
     'email': 'emily.tiernen@saaspersonas.dev',
     'title': 'CEO',
     'biography': 'I need access to applications for my Executive team and all Sales data.',
+    'image': '',
     'groups': ['Executive', 'North', 'South']
 },
 {
-    'subject': 'SaaSP-91a34244-b089-43b2-a08e-069df72eb1c5',
+    'subject': 'sportal-91a34244-b089-43b2-a08e-069df72eb1c5',
     'name': 'Landon Bozak',
     'email': 'landon.bozak@saaspersonas.dev',
     'title': 'Sales Director',
     'biography': 'I manage, plan, and forecast the North and South regions, and work closely with our Marketing team.',
+    'image': '',
     'groups': ['Sales', 'North', 'South', 'Marketing']
 },
 {
-    'subject': 'SaaSP-80ae665b-6f3c-4b7b-a020-eef0ceabd27f',
+    'subject': 'sportal-80ae665b-6f3c-4b7b-a020-eef0ceabd27f',
     'name': 'Claire Palmer',
     'email': 'claire.palmer@saaspersonas.dev',
     'title': 'Marketing Lead - South',
     'biography': 'I analyze and execute Marketing campaigns and strategy for the South Region, working closely with Sales to ensure we are successfully meeting our company goals.',
+    'image': '',
     'groups': ['Sales', 'South', 'Marketing']
 },
 {
-    'subject': 'SaaSP-653058f0-f910-44c7-9799-151f8b8e7436',
+    'subject': 'sportal-653058f0-f910-44c7-9799-151f8b8e7436',
     'name': 'Ryan Caruso',
     'email': 'ryan.caruso@saaspersonas.dev',
     'title': 'Supply Chain Manager',
     'biography': 'I execute our raw materials sourcing and purchasing strategies. I also help forecast demand and finished goods inventory for our manufacturing processes.',
+    'image': '',
     'groups': ['Supply Chain']
 },
 {
-    'subject': 'SaaSP-2cd2018a-75ee-4a38-b97e-b7f36a698f34',
+    'subject': 'sportal-2cd2018a-75ee-4a38-b97e-b7f36a698f34',
     'name': 'Jonathan Sway',
     'email': 'jonathan.sway@saaspersonas.dev',
     'title': 'Analytics COE Director',
     'biography': 'I manage our Analytics Center of Excellence, and work closely with our business stakeholders to produce actionable insights that drive value to our customers.',
+    'image': '',
     'groups': ['Qlik Developer', 'IT']
 },
 {
-    'subject': 'SaaSP-3e763aba-08b2-40b0-975d-8845c1d666e3',
+    'subject': 'sportal-3e763aba-08b2-40b0-975d-8845c1d666e3',
     'name': 'Jerry Bly',
     'email': 'jerry.bly@saaspersonas.dev',
     'title': 'External Consultant Dev',
     'biography': 'I\'ve been contracted by the company to assist in a Marketing Analytics review.',
+    'image': '',
     'groups': ['External']
 },
 {
-    'subject': 'SaaSP-6d61a4e2-bcf1-4bae-934b-0af5206f218d',
+    'subject': 'sportal-6d61a4e2-bcf1-4bae-934b-0af5206f218d',
     'name': 'Ted Graff',
     'email': 'ted.graff@saaspersonas.dev',
     'title': 'Qlik Developer',
     'biography': 'I develop, test, and productionalize Qlik applications for our business stakeholders.',
+    'image': '',
     'groups': ['Qlik Developer', 'IT']
 }
 ]";
