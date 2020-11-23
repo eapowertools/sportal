e58441ea8623f91a63a3c8faf284898aea9670ea
Jesse Paris <jesse.paris@qlik.com>
2020-11-23 15:18:03 -0800

    adding base64 images and removing png files.

--- sportal/Data/User.cs
+++ sportal/Data/User.cs
@@ -14,15 +14,18 @@ namespace sportal.Data
 
 		public string Biography { get; private set; }
 
+		public string Image { get; private set; }
+
 		public string[] Groups { get; private set; }
 
-		public User(string subject, string displayName, string email, string title, string biography, string[] groups)
+		public User(string subject, string displayName, string email, string title, string biography, string image, string[] groups)
 		{
 			this.Subject = subject;
 			this.DisplayName = displayName;
 			this.Email = email;
 			this.Title = title;
 			this.Biography = biography;
+			this.Image = image;
 			this.Groups = groups;
 		}
 	}
